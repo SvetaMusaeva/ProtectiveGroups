de3cf09f5e5b8baef76fb2228f559266fbe01e34
Ramil Nugmanov <stsouko@live.ru>
2017-01-06 17:22:37 +0300

    fix

--- PGAtools/fingerprints.py
+++ PGAtools/fingerprints.py
@@ -1,13 +1,18 @@
 from bitstring import BitArray
+from sklearn.neighbors.ball_tree import BallTree
+from sklearn.neighbors.dist_metrics import PyFuncDistance
 from .config import FP_SIZE, FP_ACTIVE_BITS
 from hashlib import md5
 
 
 def get_fingerprint(s):
-    fp = BitArray(2 ** FP_SIZE)
+    active_bits = set()
     for k, v in s.items():
         if v:
             b = BitArray(md5(k.encode()).digest())
             for r in range(FP_ACTIVE_BITS):
-                fp[b[r * FP_SIZE: (r + 1) * FP_SIZE].uint] = True
+                active_bits.add(b[r * FP_SIZE: (r + 1) * FP_SIZE].uint)
+
+    fp = BitArray(2 ** FP_SIZE)
+    fp.set(True, active_bits)
     return fp
