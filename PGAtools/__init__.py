a51dbbad330d747503d699de9965d820e3de54f1
stsouko <stsouko@live.ru>
2017-02-06 15:39:09 +0300

    fixed pony init.

--- PGAtools/__init__.py
+++ PGAtools/__init__.py
@@ -24,13 +24,19 @@
 
 def init():
     from pony.orm import sql_debug
+    from MWUI.ORM import db as mwui_db
+    from MWUI.config import (DB_NAME as MWUI_DB_NAME, DB_USER as MWUI_DB_USER, DB_HOST as MWUI_DB_HOST,
+                             DB_PASS as MWUI_DB_PASS)
     from .models import db
     from .config import DEBUG, DB_NAME, DB_DATA, DB_HOST, DB_PASS, DB_USER
 
     if DEBUG:
         sql_debug(True)
         db.bind('sqlite', 'database.sqlite')
+        mwui_db.bind('sqlite', 'mwui_database.sqlite')
     else:
         db.bind('postgres', user=DB_USER, password=DB_PASS, host=DB_HOST, database=DB_NAME)
+        mwui_db.bind('postgres', user=MWUI_DB_USER, password=MWUI_DB_PASS, host=MWUI_DB_HOST, database=MWUI_DB_NAME)
 
     db.generate_mapping(create_tables=DEBUG)
+    mwui_db.generate_mapping(create_tables=DEBUG)
